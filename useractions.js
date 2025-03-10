4c03967384f11dfa3e8c3015fc38a521b6cffe99
eddysegaud <54800904+eddysegaud@users.noreply.github.com>
2025-03-10 15:00:02 +0100

    Update useractions.js

--- useractions.js
+++ useractions.js
@@ -476,7 +476,7 @@ function createAndDownloadTempFile(content, filename, contentType) {
 function createTempfileAndAssignToButton(fileName, buttonClass, fileContent) {
 	document.getElementByClass('Bloc-dynamic-Buttons-bpmnfile').addEventListener('click', function() {
 	    const fileType = 'text/plain'; // Or 'application/json', 'image/png', etc.
-	    if((fileName.length > 0) and (buttonClass.length > 0) and (fileContent.length > 0)) {
+	    if((fileName.length > 0) && (buttonClass.length > 0) && (fileContent.length > 0)) {
 	    	createAndDownloadTempFile(fileContent, fileName, fileType);
 	    }else {
 	    	console.log('Error to generate link BPMN button');
